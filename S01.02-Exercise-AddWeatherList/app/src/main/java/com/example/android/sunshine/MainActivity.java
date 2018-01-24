1b1ab810cd30b4b1eb0ce959c0e63ead501ecf57
esugi <esugimot@ucsd.edu>
2018-01-23 22:09:53 -0800

    S01.02 Complete
    Added more fake weather data.

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -38,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
                 "Friday - Thunderstorms - 21°C / 9°C",
                 "Saturday - Thunderstorms - 16°C / 7°C",
                 "Sunday - Rainy - 16°C / 8°C",
+                "Monday - Partly Cloudy - 15°C / 10°C",
+                "Tue, May 24 - Meatballs - 16°C / 18°C",
+                "Wed, May 25 - Cloudy - 19°C / 15°C",
+                "Thu, May 26 - Stormy - 30°C / 11°C",
+                "Fri, May 27 - Hurricane - 21°C / 9°C",
+                "Sat, May 28 - Meteors - 16°C / 7°C",
+                "Sun, May 29 - Apocalypse - 16°C / 8°C",
+                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
         };
         // TODO (4) Append each String from the fake weather data array to the TextView
         for(String fakeWeather:fakeWeatherData)
